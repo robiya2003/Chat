6b0b6665ce76c6d9eb964e024090a029ab066e62
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-02-12 22:30:24 +0500

    Add project files.

---
+++ PosgreSqlWithCsharp.cs
@@ -0,0 +1,119 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    public static class PosgreSqlWithCsharp
+    {
+        public const string CONNECTINGSTRING = "Server=127.0.0.1;Port=5432;Database=telegramchat;User Id=postgres;Password=dfrt43i0";
+
+        public static bool Regisratsiya(string fullname, string username, string password)
+        {
+            if (Checking(username, password) && password.Length == 8)
+            { 
+
+            #region Registratsiya qismi
+            string query = $"insert into users(fullname,username,password1) values" +
+                $"('{fullname}','{username}','{password}')";
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection(CONNECTINGSTRING);
+            npgsqlConnection.Open();
+            NpgsqlCommand npgsqlCommand = new NpgsqlCommand(query, npgsqlConnection);
+            npgsqlCommand.ExecuteNonQuery();
+            npgsqlConnection.Close();
+            #endregion
+            return true;
+            }
+            return false;
+        }
+        public static bool Checking(string username,string password)
+        {
+            #region
+            string query = $"select * from users where username='{username}' and password1='{password}'";
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection(CONNECTINGSTRING);
+            npgsqlConnection.Open();
+            NpgsqlCommand npgsqlCommand = new NpgsqlCommand(query, npgsqlConnection);
+            NpgsqlDataReader? reader = npgsqlCommand.ExecuteReader();
+            List<object[]> ResultList = new List<object[]>();
+            while (reader.Read())
+            {
+                object[] objects = new object[reader.FieldCount];
+                reader.GetValues(objects);
+                ResultList.Add(objects);
+            }
+            npgsqlConnection.Close();
+            if(ResultList.Count == 0)
+            {
+                return true;
+            }
+            return false;
+            #endregion
+        }
+        public static List<object[]> GetAllUsers(string username, string password)
+        {
+              #region
+                string query = $"select user_id,username from users where username<>'{username}' and password1<>'{password}'";
+                NpgsqlConnection npgsqlConnection = new NpgsqlConnection(CONNECTINGSTRING);
+                npgsqlConnection.Open();
+                NpgsqlCommand npgsqlCommand = new NpgsqlCommand(query, npgsqlConnection);
+                NpgsqlDataReader? reader = npgsqlCommand.ExecuteReader();
+                List<object[]> ResultList = new List<object[]>();
+                while (reader.Read())
+                {
+                    object[] objects = new object[reader.FieldCount];
+                    reader.GetValues(objects);
+                    ResultList.Add(objects);
+                }
+                npgsqlConnection.Close();
+                return ResultList;
+                
+                
+                #endregion
+            
+            
+        }
+        public static void InserMessage(string username, string password,int id,string message)
+        {
+            #region Registratsiya qismi
+            string query = $"insert into allchats(sender_id,receiver_id,message1) values\r\n((select user_id from users where username='{username}' and password1='{password}'),{id},'{message}')\r\n";
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection(CONNECTINGSTRING);
+            npgsqlConnection.Open();
+            NpgsqlCommand npgsqlCommand = new NpgsqlCommand(query, npgsqlConnection);
+            npgsqlCommand.ExecuteNonQuery();
+            npgsqlConnection.Close();
+            #endregion
+        }
+        public static void Messagess(string username, string password)
+        {
+            #region
+            string query = $"select user_id,username,message1 from allchats\r\ninner join users on user_id=sender_id\r\nwhere receiver_id=(select user_id from users where username='{username}' and password1='{password}')";
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection(CONNECTINGSTRING);
+            npgsqlConnection.Open();
+            NpgsqlCommand npgsqlCommand = new NpgsqlCommand(query, npgsqlConnection);
+            NpgsqlDataReader? reader = npgsqlCommand.ExecuteReader();
+            List<object[]> ResultList = new List<object[]>();
+            while (reader.Read())
+            {
+                object[] objects = new object[reader.FieldCount];
+                reader.GetValues(objects);
+                ResultList.Add(objects);
+            }
+            npgsqlConnection.Close();
+            foreach (object[] column in ResultList)
+            {
+                foreach (object row in column)
+                {
+                    Console.Write(row + " -> ");
+                }
+                Console.WriteLine("");
+            }
+
+
+            #endregion
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿namespace Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CHAT();
+        }
+        public static void SignUp()
+        {
+            #region
+            Console.WriteLine("parol 8 ta harfdan iborat bolishi \nusername unikal bolishi kerak\n");
+            Console.WriteLine("Registratsiya fullname kiriting");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("Registratsiya uchun username kiriting ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Registratsiya password kiriting");
+            string password = Console.ReadLine();
+            if (PosgreSqlWithCsharp.Regisratsiya(fullname, username, password))
+            {
+                Console.WriteLine("Registartsiyadan muvaffaqiyatli otdingiz");
+                CHAT();
+            }
+            else
+            {
+                Console.WriteLine("Username yoki parol hato \nQaytadan urinib ko'ring ");
+                SignUp();
+            }
+            #endregion
+        }
+        public static void LogIn()
+        {
+            
+            Console.Clear();
+            Console.WriteLine("LOG IN");
+            Console.WriteLine("<- orqaga     oldinga->");
+            var key1 = Console.ReadKey();
+
+            if (key1.Key == ConsoleKey.LeftArrow)
+            {
+                CHAT();
+            }
+            #region
+            Console.WriteLine("username kiriting ");
+            string username = Console.ReadLine();
+            Console.WriteLine("password kiriting");
+            string password = Console.ReadLine();
+            if (!PosgreSqlWithCsharp.Checking(username, password))
+            {
+                chatsss(username, password);
+            }
+            else
+            {
+                Console.WriteLine("username yoki password hato");
+                LogIn();
+            }
+            #endregion
+        }
+        public static void CHAT()
+        {
+            #region
+            //Console.Clear();
+            Console.WriteLine("<-  SIGN UP      LOG IN  ->");
+            var key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.LeftArrow)
+            {
+                SignUp();
+            }
+            else if (key.Key == ConsoleKey.RightArrow)
+            {
+                LogIn();
+            }
+            #endregion
+        }
+        public static void Message(string username,string password)
+        {
+            Console.WriteLine("Yozmoqchi bolgan userni idsini kiriting");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Message kiriting ");
+            string message = Console.ReadLine();
+            PosgreSqlWithCsharp.InserMessage(username, password, id,message);
+            Console.WriteLine("message jonatildi");
+            chatsss(username, password);
+
+        }
+        public static void chatsss(string username, string password)
+        {
+            Console.Clear();
+            Console.WriteLine("<- orqaga     Message->");
+            Console.WriteLine("Messges-----------------------------------");
+            PosgreSqlWithCsharp.Messagess(username, password);
+            Console.WriteLine("ALL USERS----------------------------------");
+            List<object[]> ResultList = PosgreSqlWithCsharp.GetAllUsers(username, password);
+            foreach (object[] column in ResultList)
+            {
+                foreach (object row in column)
+                {
+                    Console.Write(row + "  ");
+                }
+                Console.WriteLine("");
+            }
+
+            var key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.LeftArrow)
+            {
+                LogIn();
+            }
+            else if (key.Key == ConsoleKey.RightArrow)
+            {
+                Message(username, password);
+            }
+        }
+    }
+}
